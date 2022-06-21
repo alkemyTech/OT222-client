a22ce1a9deec5800cb849e811ad00608cdabc7d3
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-06-21 20:17:22 -0300

    Email regex updated to match LoginForm.jsx regex

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -18,7 +18,7 @@ const validate = values => {
 
     if (!values.email) {
         errors.email = "Obligatorio"
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    } else if (!/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i.test(values.email)) {
         errors.email = "Direccción de correo inválida"
     }
 
