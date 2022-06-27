cb795de6c12e9755a3b452bc69b63de765054fa6
LuchoQQ <lucianosanchez125@hotmail.com>
2022-06-26 23:12:48 -0300

    bugs & names fixed

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -4,7 +4,7 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     isLogged: false,
-    userData: {},
+    user: {},
   },
   reducers: {
     login: (state) => {
@@ -12,21 +12,22 @@ export const userSlice = createSlice({
     },
     logout: (state) => {
       state.isLogged = false;
+      state.user = {};
     },
     handleUser: (state, action) => {
-      state.userData = state.userData = action.payload;
+      state.user = action.payload;
     },
   },
 });
 
 export const { login, logout, handleUser } = userSlice.actions; // user actions
 
-export const selectUser = state => state.user.isLogged // return user status 
+export const selectUserStatus = (state) => state.user.isLogged; // return user status
 
-export const showUser = state => state.user.userData // return user data
+export const selectUser = (state) => state.user.user; // return user data
 
-export const setUserAsync = (user) => dispatch => {
-    dispatch(handleUser(user))
-}
+export const setUserAsync = (user) => (dispatch) => {
+  dispatch(handleUser(user));
+};
 
-export default userSlice.reducer
+export default userSlice.reducer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Box, Button, Grid, Text } from '@chakra-ui/react'
+import { login, logout, handleUser, selectUser, selectUserStatus } from '../features/user/userSlice'
+import { useSelector, useDispatch } from 'react-redux';
 
 const HomePage = () => {
+  const user = useSelector(selectUser)
+  const isLogged = useSelector(selectUserStatus)
+  const dispatch = useDispatch()
+  console.log(user)
+  console.log(isLogged)
   return (
-    <div>HomePage</div>
+    <>
+      HomePage
+      <Text>{user === true ? 'true' : 'false'}</Text>
+      <Button onClick={() => dispatch(login())}>login</Button>
+      <Button onClick={() => dispatch(logout())}>logout</Button>
+      <Button onClick={() => dispatch(handleUser({
+        name: 'lucho',
+        email: 'lasdijawod'
+      }))}>setUser</Button>
+    </>
   )
 }
 
