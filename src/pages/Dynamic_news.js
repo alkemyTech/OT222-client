cef9c116fb4975bca0cd44176976516b00d7eec5
LuchoQQ <lucianosanchez125@hotmail.com>
2022-06-28 16:40:21 -0300

    OT222-49 Mobile version

--- src/pages/Dynamic_news.js
+++ src/pages/Dynamic_news.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { Grid, Flex, Text, Image, Box } from "@chakra-ui/react";
+import { Grid, Flex, Text, Image, Icon } from "@chakra-ui/react";
 import axios from "axios";
 import banner from "../assets/newsBanner.png";
-
+import { TbError404 } from "react-icons/tb";
 const Dynamic_news = () => {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
@@ -17,20 +17,31 @@ const Dynamic_news = () => {
     };
     fetchData();
   }, []);
-
   return (
     <>
-      <Flex h="auto" w="100vw" flexDir={"column"}>
-        <Image src={data.img || banner} fit={"cover"} />
-        <Text fontSize={"6xl"} textAlign="center" fontWeight={'500'}>
-          {data.name}
-        </Text>
-        <Flex w='100vw' justifyContent={'center'} my='15vh'>
-          <Text w={["70vw", "50vw"]} fontSize={['xl','2xl']} h='auto'>
-            {data.content}
+      {data.message !== "Not Found" ? (
+        <Grid
+          h="50vw"
+          w="100vw"
+          justifyContent={"center"}
+          alignContent="center"
+        >
+          <Icon as={TbError404} w={['100px','200px']} h={['100px','200px']}justifySelf={'center'}/>
+          <Text fontSize={["3xl","6xl"]}>ERROR 404: New Not Founded!.</Text>
+        </Grid>
+      ) : (
+        <Flex h="auto" w="100vw" flexDir={"column"}>
+          <Image src={data.img || banner} fit={"cover"} />
+          <Text fontSize={"6xl"} textAlign="center" fontWeight={"500"}>
+            {data.name}
           </Text>
+          <Flex w="100vw" justifyContent={"center"} my="15vh">
+            <Text w={["70vw", "50vw"]} fontSize={["xl", "2xl"]} h="auto">
+              {data.content}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
+      )}
     </>
   );
 };
