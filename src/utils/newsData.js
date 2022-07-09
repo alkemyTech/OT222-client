d26776629b079ee96526d5a608179583508ba4d2
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-09 14:59:17 -0500

    newData created in utils folder in order to apply the backoffice news

---
+++ src/utils/newsData.js
@@ -0,0 +1,26 @@
+const newsData = [
+  {
+    _id: "AAAA0011",
+    image:
+      "https://www.fundacion-affinity.org/sites/default/files/dia-de-los-animales.jpg",
+    name: "Aca escribo un titulo que sea mas o menos largo como para probar",
+    alt: "foto1",
+    createdAt: new Date(),
+  },
+  {
+    _id: "AAAA0100",
+    image:
+      "https://assets.afcdn.com/story/20161017/989289_w1200h630c1cx511cy250.jpg",
+    name: "Aca escribo un titulo que sea mas o menos largo como para probar",
+    alt: "foto2",
+    createdAt: new Date(),
+  },
+  {
+    _id: "AAAA0101",
+    image: "https://okdiario.com/img/2018/11/20/leopardo.jpg",
+    name: "Aca escribo un titulo que sea mas o menos largo como para probar",
+    alt: "foto3",
+    createdAt: new Date(),
+  },
+]
+export default newsData
