8638837e81f62cd7f470a2cc8d5f634ed9a225e0
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-09 16:21:20 -0500

    fixing the date format

--- src/utils/newsData.js
+++ src/utils/newsData.js
@@ -5,7 +5,9 @@ const newsData = [
       "https://www.fundacion-affinity.org/sites/default/files/dia-de-los-animales.jpg",
     name: "Aca escribo un titulo que sea mas o menos largo como para probar",
     alt: "foto1",
-    createdAt: new Date(),
+    briefDescription:
+      "Aca escribo una descricion que sea mas o menos larga como para probar",
+    createdAt: new Date().toLocaleString(),
   },
   {
     _id: "AAAA0100",
@@ -13,14 +15,18 @@ const newsData = [
       "https://assets.afcdn.com/story/20161017/989289_w1200h630c1cx511cy250.jpg",
     name: "Aca escribo un titulo que sea mas o menos largo como para probar",
     alt: "foto2",
-    createdAt: new Date(),
+    briefDescription:
+      "Aca escribo una descricion que sea mas o menos larga como para probar",
+    createdAt: new Date().toLocaleString(),
   },
   {
     _id: "AAAA0101",
     image: "https://okdiario.com/img/2018/11/20/leopardo.jpg",
     name: "Aca escribo un titulo que sea mas o menos largo como para probar",
     alt: "foto3",
-    createdAt: new Date(),
+    briefDescription:
+      "Aca escribo una descricion que sea mas o menos larga como para probar",
+    createdAt: new Date().toLocaleString(),
   },
 ]
 export default newsData
