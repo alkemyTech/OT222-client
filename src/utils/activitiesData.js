5a4e86577107c9892acf411dd937473bc3b9f159
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-10 01:00:06 -0500

    adding details field in activitiesData.js

--- src/utils/activitiesData.js
+++ src/utils/activitiesData.js
@@ -2,18 +2,22 @@ const activitiesData = [
   {
     _id: "AAAA0000",
     name: "Activity 1",
+    details: "This is a detailed description of the activity 1",
   },
   {
     _id: "AAAA0001",
     name: "Activity 2",
+    details: "This is a detailed description of the activity 2",
   },
   {
     _id: "AAAA0002",
     name: "Activity 3",
+    details: "This is a detailed description of the activity 3",
   },
   {
     _id: "AAAA0003",
     name: "Activity 4",
+    details: "This is a detailed description of the activity 4",
   },
 ]
 export default activitiesData
