d6988c57f0f7f85397c236ab39feadabf369fa10
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-10 00:10:33 -0500

    activities data in utils folder

---
+++ src/utils/activitiesData.js
@@ -0,0 +1,19 @@
+const activitiesData = [
+  {
+    _id: "AAAA0000",
+    name: "Activity 1",
+  },
+  {
+    _id: "AAAA0001",
+    name: "Activity 2",
+  },
+  {
+    _id: "AAAA0002",
+    name: "Activity 3",
+  },
+  {
+    _id: "AAAA0003",
+    name: "Activity 4",
+  },
+]
+export default activitiesData
