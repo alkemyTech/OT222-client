4012e4751e362d290cb1d71aed1fa8aecb816783
LuchoQQ <lucianosanchez125@hotmail.com>
2022-07-17 17:11:26 -0300

    contact form connect with endpoint /contacts

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import ContactForm from '../components/ContactForm'
-import ContactsTable from '../components/ContactsTable/ContactsTable'
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import ContactsTable from "../components/ContactsTable/ContactsTable";
 
 const ContactPage = () => {
   return (
-  /*   <ContactForm /> */
-    <ContactsTable />
-  )
-}
+    <>
+      <ContactForm />
+      <ContactsTable />
+    </>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
