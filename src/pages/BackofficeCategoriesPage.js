d1bdb2ff0cf568184d6ec6bde65254a33afa9ae3
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-17 02:12:11 -0500

    categories in backoffice page created, get the categories list in a table from server

--- src/pages/BackofficeCategoriesPage.js
+++ src/pages/BackofficeCategoriesPage.js
@@ -1,5 +1,42 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+// Components
+import TableComponent from "../components/TableComponent"
+import LoaderSpinner from "../components/LoaderSpinner"
+// API
+import API from "../API"
 const BackofficesCategoriesPage = () => {
-  return <>Categories</>
+  // arrs
+  const [categoriesName, setCategoriesName] = useState(Array)
+  // strs
+  const [errorMessage, setErrorMessage] = useState(String)
+
+  // bools
+  const [spinner, setSpinner] = useState(Boolean)
+  const [error, setError] = useState(Boolean)
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        setSpinner(true)
+        setError(false)
+        const { categoriesName } = await API.getCategories()
+        if (!categoriesName) return setError(true)
+        setSpinner(false)
+        setCategoriesName(categoriesName)
+      } catch (err) {
+        setErrorMessage(err.message)
+        setError(true)
+      }
+    }
+    getCategories()
+  }, [])
+
+  return (
+    <>
+      {spinner && <LoaderSpinner />}
+      {error && <p>{errorMessage}</p>}
+      <TableComponent tableHeaders={["Categories"]} data={categoriesName} />
+    </>
+  )
 }
 export default BackofficesCategoriesPage
