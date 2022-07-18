d21b52c8ce2a14d6c41f69b1430cab11fa720cb3
LuchoQQ <lucianosanchez125@hotmail.com>
2022-07-18 01:13:38 -0300

    OT222-93

--- src/components/MemberThumbnail.js
+++ src/components/MemberThumbnail.js
@@ -15,7 +15,7 @@ const MemberThumbnail = ({ name, image, role }) => {
           
         ></Image>
         <Flex w="100%" h="100%" position='absolute' top='0' justifyContent='center' alignContent='center'>
-            <Box mt='auto'>
+            <Box mt='auto' mb='2vh'>
                 <Text textAlign='center' fontSize='3xl' fontWeight='semibold' color='#fff' textShadow='1px 2px #000'>{name}</Text>
             </Box>
         </Flex>
