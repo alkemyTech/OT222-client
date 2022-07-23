e5a88950a505937b5223a34784fceabb2759fbd2
MagdalenaCasas <maguicasas33@gmail.com>
2022-07-22 21:14:07 -0300

    commit
    post de imagen a traves del formulario. Tener en cuenta que el post tiene la url escrita y no como variable

--- src/components/EditOrganizationForm.js
+++ src/components/EditOrganizationForm.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { Formik, Form, Field }from "formik";
-import {Flex, Input,Button,Text} from '@chakra-ui/react';
+import {Flex, Input,Button,Text, useFormControlProps} from '@chakra-ui/react';
 import * as yup from "yup";
 import KErrorMessage from "../components/KErrorMessage"  
 
@@ -15,7 +15,29 @@ const validationSchema = yup.object({
   
 });  
 
-const EditOrganizationForm = () =>{    
+
+const EditOrganizationForm = () =>{ 
+  // para usar en el postde la imagen
+  const[file, setFile] = useState();
+  const[fileName, setFileName] = useState();
+
+
+ const postImage = ()=>{
+    var formdata = new FormData();
+    formdata.append("file", file, fileName);
+    formdata.append("key", fileName);
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    fetch("http://localhost:3001/files", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }  
     return(    
         <Flex
         flexDirection={"column"}
@@ -26,16 +48,19 @@ const EditOrganizationForm = () =>{
            name:"",
            logo:"",
           }}
+
           onSubmit={(values) => {
             console.log(values);
+            postImage();
           }}
         >
-          {() => (
+          {(props) => (
             <Flex as={Form}
             className="form"
             flexDirection={"column"}
             width={"60%"}
             ml={"5%"}
+            mb={30}
             gap={"5"}
             >
                 <Text
@@ -56,6 +81,12 @@ const EditOrganizationForm = () =>{
                 type = "file"
                 name= "logo"
                 accept="image/x-png,image/gif,image/jpeg"
+                onChange ={(e)=>{
+                  console.log(e.currentTarget.files[0])
+                  setFile(e.currentTarget.files[0]);
+                  setFileName(e.currentTarget.files[0].name);
+                  props.handleChange(e)
+                }}
                 />
                 <KErrorMessage name="logo"/>
 
