e300230f00ba303cf6a9a2b955a943df20109dc1
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-24 18:26:30 -0500

    setting the sign up new user url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // URL
 const BASE_URL =
   process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:8080"
+const SIGN_UP_URL = `auth/register`
 const BACKOFFICE_CATEGORIES_URL = `${BASE_URL}/categories`
-export { BACKOFFICE_CATEGORIES_URL }
+export { SIGN_UP_URL, BACKOFFICE_CATEGORIES_URL }
