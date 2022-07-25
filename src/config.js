d6539b1ff81b53bfa48e12aa4794f6d9fa2aed07
Alvaro Avalos <tlalvaro15@gmail.com>
2022-07-24 23:03:49 -0500

    adding organization public url in config file for serve in the api client

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@
 const BASE_URL =
   process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:8080"
 const SIGN_UP_URL = `auth/register`
+const ORGANIZATIONS_PUBLIC_URL = `${BASE_URL}/organizations/public`
 const BACKOFFICE_CATEGORIES_URL = `${BASE_URL}/categories`
-export { SIGN_UP_URL, BACKOFFICE_CATEGORIES_URL }
+export { SIGN_UP_URL, ORGANIZATIONS_PUBLIC_URL, BACKOFFICE_CATEGORIES_URL }
