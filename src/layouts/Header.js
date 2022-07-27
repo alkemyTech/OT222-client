69488a2052d15a9067beda87697b28c4c86b996c
Jeronimo Balestra <jeronimobalestra@gmail.com>
2022-07-27 15:49:46 -0300

    Fixed "cerrar sesión" button not showing up on mob

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import { Box, Flex, Grid, Icon, Image, Button } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 import NavButton from "../components/NavButton";
 import axios from "axios";
 import logoIcon from "../public/LOGO-SOMOS-MAS.png";
 import { CgMenu } from "react-icons/cg";
 import { AiOutlineClose } from "react-icons/ai";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom'
+import { logout } from '../features/user/userSlice'
+
 const Header = () => {
   const [mobile, setMobile] = useState(false);
   const [publicAssets, setPublic] = useState({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const logoutFunc = (dispatch, navigate) => {
+    localStorage.removeItem('token');
+    dispatch(logout());
+    navigate('/');
+    document.location.reload();
+  };
 
   // try get logo from endpoint
   const getLogo = async () => {
@@ -86,19 +99,27 @@ const Header = () => {
         </Flex>
         <Navbar setMobile={setMobile} mobile={mobile} />
         <Grid gap="6vh">
-          <NavButton
-            name="Log In"
-            path="/login"
-            mobile={false}
-            setMobile={setMobile}
-          />
-          <NavButton
-            name="Registrarse"
-            path="/register"
-            color="red"
-            mobile={false}
-            setMobile={setMobile}
-          />
+          {useSelector(state => state.user.isLogged) ? (
+            <Button variant="link" onClick={() => logoutFunc(dispatch, navigate)}>
+              Cerrar Sesión
+            </Button>
+          ) : (
+            <>
+              <NavButton
+                name="Log In"
+                path="/login"
+                mobile={false}
+                setMobile={setMobile}
+              />
+              <NavButton
+                name="Registrarse"
+                path="/register"
+                color="red"
+                mobile={false}
+                setMobile={setMobile}
+              />
+            </>
+          )}
         </Grid>
       </Grid>
       <Box h="15vh" />
